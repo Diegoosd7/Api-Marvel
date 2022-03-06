f1cdca1fcbeabd084f0769aa8b67817256eaaaa3
Diego Sánchez <disandu07@gmail.com>
2022-03-06 14:11:59 +0100

    He añadido las carpetas Comic, Comic List, Pj, Pj list. Modificado footer

--- src/App/components/Footer/Footer.jsx
+++ src/App/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 import facebook from '../../img/logos/fb.png';
 import twitter from '../../img/logos/tw.png';
@@ -12,19 +12,19 @@ const Footer = () => {
     return (
         <footer>
             <center>
-                    <a href="https://www.facebook.com/marvel" target="_blank" rel="noopener noreferrer">
-                        <img className="img" src={facebook} alt="facebook" width="50px" height="50px" />
+                <a href="https://www.facebook.com/marvel" target="_blank" rel="noopener noreferrer">
+                    <img className="footer--image" src={facebook} alt="facebook" width="50px" height="50px" />
                 </a>
                 <a href="https://www.twitter.com/marvel" target="_blank" rel="noopener noreferrer">
-                        <img className="img" src={twitter} alt="facebook" width="50px" height="50px" />
+                    <img className="footer--image" src={twitter} alt="twitter" width="50px" height="50px" />
                 </a>
                 <a href="https://www.instagram.com/marvel" target="_blank" rel="noopener noreferrer">
-                        <img className="img ig" src={instagram} alt="facebook" width="50px" height="50px" />
+                    <img className="footer--image" src={instagram} alt="instagram" width="50px" height="50px" />
                 </a>
                 <a href="https://www.youtube.com/marvel" target="_blank" rel="noopener noreferrer">
-                        <img className="img" src={youtube} alt="facebook" width="50px" height="50px" />
+                    <img className="footer--image" src={youtube} alt="youtube" width="50px" height="50px" />
                 </a>
-                <p>@2022 Marvel, Inc. All Rights Reserved. </p>
+                <p>©2022 MARVEL</p>
             </center>
         </footer>
     )
