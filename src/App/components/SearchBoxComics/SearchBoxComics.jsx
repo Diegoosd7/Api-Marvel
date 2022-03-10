0c087362069b6cceb94cccf58b6aed1e057c6e70
Diego Sánchez <disandu07@gmail.com>
2022-03-10 22:46:02 +0100

    SearchBoxComics funcionando

--- src/App/components/SearchBoxComics/SearchBoxComics.jsx
+++ src/App/components/SearchBoxComics/SearchBoxComics.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './SearchBoxComics.css';
 
-function SearchBoxComics({ search }) {
-  // const [text, setText] = useState('')
-  // const onSearch = (q) => {
-  //   setText(q)
-  //   search(q)
-  // }
+function SearchBoxComics({search}) {
+  const [text, setText] = useState('')
+  const onSearch = (q) => {
+    setText(q)
+    search(q)
+  }
 
   return (
-    <center>
-      <input id="search-comic" type="search"
-        placeholder="Busca un comic"
-        autoFocus/>
-      {/* <i class="bi bi-search" id="icono4"/> */}
-    </center>
+      <center>
+        <input id="search" type="search"
+          placeholder="Busca un comic  🔍"
+          autoFocus
+          onChange={(e) => onSearch(e.target.value)}
+          value={text} />
+        {/* <i class="bi bi-search" id="icono4"/> */}
+      </center>
   )
 }
 
