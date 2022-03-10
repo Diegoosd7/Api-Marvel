212279cec70c40fc920c1116fabb606c6486149c
Diego Sánchez <disandu07@gmail.com>
2022-03-10 20:17:59 +0100

    SearchBox Personajes Bonito

--- src/App/components/SearchBoxPersonajes/SearchBoxPersonajes.jsx
+++ src/App/components/SearchBoxPersonajes/SearchBoxPersonajes.jsx
@@ -1,27 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import './SearchBoxPersonajes.css';
 
 function SearchBoxPersonajes({ search }) {
-  const [text, setText] = useState('');
+  const [text, setText] = useState('')
   const onSearch = (q) => {
-    setText(q);
-    search(q);
-  };
+    setText(q)
+    search(q)
+  }
 
   return (
-    <center>
-      <input
-        id="search"
-        type="search"
-        placeholder="Busca un personaje  🔍"
-        autoFocus
-        onChange={(e) => onSearch(e.target.value)}
-        value={text}
-      />
-      {/* <i class="bi bi-search" id="icono4"/> */}
-    </center>
+      <center>
+        <input id="search" type="search"
+          placeholder="Busca un personaje                                                                                                     🔍"
+          autoFocus
+          onChange={(e) => onSearch(e.target.value)}
+          value={text} />
+        {/* <i class="bi bi-search" id="icono4"/> */}
+      </center>
 
-  );
+
+  )
 }
 
 export default SearchBoxPersonajes;
