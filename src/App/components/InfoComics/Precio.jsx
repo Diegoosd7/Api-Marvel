ce75914472ecf9e26d6053af5d646a8deea8a9d3
Diego Sánchez <disandu07@gmail.com>
2022-03-11 23:54:14 +0100

    Informacion Personajes y Comics definitiva

--- src/App/components/InfoComics/Precio.jsx
+++ src/App/components/InfoComics/Precio.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Precio({ precios }) {
-    return (precios.map(item => <p className="informacion__div__p">{item.price} €</p>))
+    return (precios.map(item => {
+        let url = item.type
+        let enlace = url.toUpperCase()
+        return (<p className="informacion__div__p">- {enlace}: {item.price} €</p>)
+    }))
 }
 
 export default Precio
--- src/App/components/InfoComics/URL.jsx
+++ src/App/components/InfoComics/URL.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 function URL({ enlaces }) {
-    return(enlaces.map(item =>
-    <center><a href={item.url} target="_blank" className="info__a">Enlace a la web ({item.type})<br/><br/></a></center>))
+    return (enlaces.map(item => {
+        let url = item.type
+        let enlace = url.toUpperCase()
+        return (
+            <center><a href={item.url} target="_blank" className="informacion__div__li info__a">{enlace}<br /><br /></a></center>
+        )
+    }
+    ))
 }
 
 export default URL
