efb9a91f71f22516fbdc5bc8b925e701b1e2ce8f
Diego Sánchez <disandu07@gmail.com>
2022-03-12 15:54:41 +0100

    Documentación Rutas y Servicios

--- src/index.jsx
+++ src/index.jsx
@@ -19,13 +19,15 @@ render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="comics" element={<Comics />} />
-        <Route path="personajes" element={<Personajes />} />
-        <Route path="personajeseleccionado/:idpersonaje" element={<PersonajeSelect />} />
-        <Route path="comicseleccionado/:idcomic" element={<ComicSelect />}>
+        <Route index element={<Home />} /> {/* Ruta que se ejecuta al iniciar la aplicación. Te carga el Home con la info de Marvel */}
+        <Route path="home" element={<Home />} /> {/* Ruta a la que te lleva el enlace de Inicio */}
+        <Route path="comics" element={<Comics />} /> {/* Ruta a la que te lleva el enlace de Comics. Carga la lista de Comics  */}
+        <Route path="personajes" element={<Personajes />} /> {/* Ruta a la que te lleva el enlace de Personajes. Carga la lista de Personajes */}
+        <Route path="personajeseleccionado/:idpersonaje" element={<PersonajeSelect />} /> {/* Ruta a la que te lleva al pinchar en un Personaje */}
+        <Route path="comicseleccionado/:idcomic" element={<ComicSelect />}> {/* Ruta a la que te lleva al pinchar en un Comic */}
         </Route>
+
+        {/* RUTA DE ERROR. SE EJECUTA CUANDO PONES UNA RUTA QUE NO EXISTE */}
         <Route
           path="*"
           element={(
--- src/services/personajes.js
+++ src/services/personajes.js
@@ -1,17 +1,25 @@
+/*
+Para hacer la llamada a la API necesitamos 5 cosas:
+-Url Base: http://gateway.marvel.com/v1/public/
+-Clave Pública
+-Clave Privada
+-TS: 1 (va a ser 1)
+-Hash: Es una suma de TS + Clave Privada + Clave Pública. Luego con un convertidor a HASHMD5 online, la encriptas
+
+URL FINAL: http://gateway.marvel.com/v1/public/comics?ts=1&apikey=${publicKey}&hash=${hash}
+
+*/
+
 // Clave Pública: 1928dbc9bba11631437d27c1258a8e7a
 // Clave Privada: fcf7345eb9fe3697999b6dbc2df107826d739ceb
-// Ts: 1
-
-// HASH= TS + CLAVE PRIVADA + CLAVE PUBLICA
 // CLAVE A ENCRIPTAR= 1fcf7345eb9fe3697999b6dbc2df107826d739ceb1928dbc9bba11631437d27c1258a8e7a
-
-//  CON UN CONVERTIDOR ONLINE
-// MD5 = 79b39bc45ede5e3689d0b2c12862b630
+// HASH = 79b39bc45ede5e3689d0b2c12862b630
 
 const hash = '79b39bc45ede5e3689d0b2c12862b630';
 const publicKey = '1928dbc9bba11631437d27c1258a8e7a';
 const URLPersonajes = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publicKey}&hash=${hash}&limit=36`;
 
+//FUNCIÓN QUE DEVUELVE 36 PERSONAJES
 export async function accedePersonajes() {
   let datos = await fetch(URLPersonajes);
   let personajes = await datos.json();
@@ -19,6 +27,7 @@ export async function accedePersonajes() {
   return personajes.data.results;
 }
 
+//FUNCIÓN QUE DEVUELVE 36 PERSONAJES O MENOS QUE EMPIECEN CON LO QUE LE PONGAS EN EL BUSCADOR
 export async function buscarPersonajes(nombre) {
   let data = await fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${nombre}&ts=1&apikey=${publicKey}&hash=${hash}&limit=36`)
   let personajes = await data.json();
@@ -26,6 +35,7 @@ export async function buscarPersonajes(nombre) {
   return personajes.data.results;
 }
 
+//FUNCIÓN QUE BUSCA EL PERSONAJE QUE HAS PINCHADO SEGÚN EL ID QUE LE PASAS
 export async function infoPersonaje(id){
   let data = await fetch (`http://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${publicKey}&hash=${hash}`);
   let personaje = await data.json();
