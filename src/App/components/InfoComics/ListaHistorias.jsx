b2c3080e57cb030227ed2b051f59eb152b6a5671
Diego Sánchez <disandu07@gmail.com>
2022-03-13 17:55:42 +0100

    estilos lintern

--- src/App/components/InfoComics/ListaHistorias.jsx
+++ src/App/components/InfoComics/ListaHistorias.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React from 'react';
 
 function ListaHistorias({ historias }) {
-    //RECIBIMOS EL ARRAY QUE NOS HAN PASADO A TRAVÉS DE UNA PROP
-    return(
-        <ul>
-            {historias.map(item =><li className="informacion__div__li">{item.name}</li>)}
-            {/*Recorremos el array y mostramos el nombre de la historia relacionada*/}
-   
-       </ul>
-       )
+  // RECIBIMOS EL ARRAY QUE NOS HAN PASADO A TRAVÉS DE UNA PROP
+  return (
+    <ul>
+      {historias.map((item) => <li className="informacion__div__li">{item.name}</li>)}
+      {/* Recorremos el array y mostramos el nombre de la historia relacionada */}
 
+    </ul>
+  );
 }
 
 /* NO PONGO EL PROPTYPE DE STORIES PORQUE EL ARRAY TIENE MUCHOS DATOS */
 
-export default ListaHistorias
+export default ListaHistorias;
